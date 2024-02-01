d148ef86898b87731b681f62925c958171c422d4
Clay O'Dell <clay.odell.music@gmail.com>
2024-01-31 18:23:17 -0600

    hasOddNumber and hasAZero added

--- someAndevery.js
+++ someAndevery.js
@@ -13,3 +13,21 @@ function myEvery(array, callback) {
         return true;
     }
     
+//some exercises
+function hasOddNumber (arr) {
+    return arr.some(function(num) {
+        return num % 2!== 0;
+    });
+    return arr;
+}
+const answer1 = hasOddNumber([1,2,2,2,2,2,4]) // true
+const answer2 = hasOddNumber([2,2,2,2,2,4]) // false
+
+function hasAZero(arr) {
+    return arr.some(function(num) {
+        return num === 0;
+    });
+    return arr;
+}
+const answer3 = hasAZero(33321232131012) // true
+const answer4 =hasAZero(1212121) // false
